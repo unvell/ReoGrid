1f3924fbf026c15c8e2aef1a6c41d2de97b84c72
Jing <dujid0@gmail.com>
2017-05-27 00:39:36 +0900

    update build script version number to 2.0.2

--- build-release.bat
+++ build-release.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET ERRORLEVEL=0
-SET VER=2.0.0
+SET VER=2.0.2.0
 
 REM #SET MSB="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild"
 SET MSB="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
