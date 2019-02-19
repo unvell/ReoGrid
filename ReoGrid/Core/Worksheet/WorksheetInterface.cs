b86781d092d530e39871be2e80b110d3af5e568c
Jing <dujid0@gmail.com>
2019-02-19 18:01:31 +0900

    add copyright

--- ReoGrid/Core/Worksheet/WorksheetInterface.cs
+++ ReoGrid/Core/Worksheet/WorksheetInterface.cs
@@ -1,4 +1,22 @@
-﻿#if PRINT
+﻿/*****************************************************************************
+ * 
+ * ReoGrid - .NET Spreadsheet Control
+ * 
+ * http://reogrid.net/
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ *
+ * Author: Jing <lujing at unvell.com>
+ *
+ * Copyright (c) 2012-2016 Jing <lujing at unvell.com>
+ * Copyright (c) 2012-2016 unvell.com, all rights reserved.
+ * 
+ ****************************************************************************/
+
+#if PRINT
 
 using System;
 using System.Collections.Generic;
